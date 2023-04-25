923b22c05416b7b811b5514ab6b22206c6e89e83
kaskil12 <kasperkilde2@gmail.com>
2023-04-25 09:26:38 +0200

    fikset noen få ting

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public static int ClotesGathered;
+    public Text ClotesGatheredText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,12 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ClotesGatheredText.text = "Clothes Gathered: " + ClotesGathered.ToString();
+    }
+    public void SpeedUpgrade(){
+
+    }
+    public void PickupSpeedUpgrade(){
         
     }
 }
