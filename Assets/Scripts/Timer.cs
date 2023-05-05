ad0b90367394fb8cb22e6420329905984ffd4a2f
kaskil12 <kasperkilde2@gmail.com>
2023-05-05 22:34:03 +0200

    Update Timer.cs

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,10 +6,11 @@ public class Timer : MonoBehaviour
 {
     public static float Timers;
     public static bool stopTimer;
+ 
     // Start is called before the first frame update
     void Start()
     {
-        Timers = 150;
+        
     }
 
     // Update is called once per frame
